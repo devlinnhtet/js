8378d5ee3b447f0d4b2ea4c835864b6c995fe05e
linn <linn.htet@outlook.com>
2024-05-08 14:45:29 +0800

    add ep-008

---
+++ section-02/ep-008/app.js
@@ -0,0 +1,26 @@
+// Boolean
+console.log(true);
+let ThisIsBoolean = true;
+console.log(typeof ThisIsBoolean);
+
+// Number
+console.log(23);
+console.log(23.44);
+let ThisIsNumber = 23;
+console.log(typeof ThisIsNumber);
+
+// String
+console.log('hello');
+console.log('hello, world');
+let ThisIsString = 'hello, world';
+console.log(typeof ThisIsString);
+
+// Null
+console.log(null);
+let ThisIsNull = null;
+console.log(typeof ThisIsNull); // object
+
+// Undefined
+console.log(undefined);
+let ThisIsUndefined;
+console.log(typeof ThisIsUndefined);
