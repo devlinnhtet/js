db3ab3d37b0898dbaf1783da5ddd9140bf68d381
linn <linn.htet@outlook.com>
2024-05-08 15:14:10 +0800

    add ep-010

---
+++ section-02/ep-010/app.js
@@ -0,0 +1,8 @@
+let email = 'alice@gmail.com';
+
+console.log(email.lastIndexOf('i'));
+console.log(email.slice(0, 5));
+console.log(email.slice(6, 11));
+console.log(email.slice(4, -3));
+console.log(email.substring(0, 5));
+console.log(email.replace('a', 'b'));
