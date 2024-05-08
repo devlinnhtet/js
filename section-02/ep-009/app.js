5a0c951adb6264bd4fde581cfff7921d4c76d544
linn <linn.htet@outlook.com>
2024-05-08 14:58:47 +0800

    add ep-009

---
+++ section-02/ep-009/app.js
@@ -0,0 +1,28 @@
+// String
+console.log('hello, world');
+let email = 'alice@gmail.com';
+console.log(email);
+
+// String Concatenation
+let firstName = 'hello';
+let lastName = 'world';
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[1]);
+
+// String length
+console.log(fullName.length);
+console.log(firstName.length);
+console.log(lastName.length);
+
+// String Methods
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
